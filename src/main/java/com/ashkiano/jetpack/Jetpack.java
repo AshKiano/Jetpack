c435cfa323956c05545ba7badb1c174f0cba65d5
AshKiano <ash.kiano@gmail.com>
2023-07-24 21:23:33 +0200

    version 1.1

--- src/main/java/com/ashkiano/jetpack/Jetpack.java
+++ src/main/java/com/ashkiano/jetpack/Jetpack.java
@@ -3,7 +3,6 @@ package com.ashkiano.jetpack;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//TODO přidat bstats 19172
 //TODO přidat plamínky když se použije
 //TODO udělat možnost aby jetpack spotřebovával třeba lávu nebo něco jiného
 //TODO možnost craftitelného paliva se speciálním lore, které bude spotřebovávat
@@ -18,5 +17,7 @@ public class Jetpack extends JavaPlugin {
         permission = config.getString("command_permission", "jetpack");
         getCommand("jetpack").setExecutor(new JetpackCommand(permission));
         getServer().getPluginManager().registerEvents(new JetpackListener(this), this);
+
+        Metrics metrics = new Metrics(this, 19172);
     }
 }
